f435749f997ae9be6d49ef20606e588d4bc3a47d
Bruna Marques <79940269+Bru-marques@users.noreply.github.com>
2024-11-07 09:05:01 +0000

    handling connections errors

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
+try{
   const result = await client.query(queryObject);
-  await client.end();
-
   return result;
-}
+
+}catch(error) {
+  console.error('Error: ', error)
+
+}finally {
+  await client.end();
+}}
 
 export default {
   query: query,
