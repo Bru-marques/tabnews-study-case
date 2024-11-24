e5af049e12b598d79627c9053ecf5a8df703b611
Bruna Marques <79940269+Bru-marques@users.noreply.github.com>
2024-11-24 09:47:37 +0000

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>A portifolio project </h1>
-      <h2>- home</h2>
-      <h2>- projects</h2>
-      <h2>- articles</h2>
-      <br />
+      <h1>TabNews - study case</h1>
       <p>
-        Qual o menor custo que eu posso ter para descobrir o melhor caminho?
+        A study case project to practice coding skills.
       </p>
     </div>
   );
